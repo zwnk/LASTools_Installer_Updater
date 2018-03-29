2cc9eb469f54da47f775dfae8feb5af5cf81ce3d
David Friedrich <david.friedrich@digimapas.cl>
2018-03-29 11:54:34 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# LASTools_Installer_Updater
+
+A simple script for installing and updating [LASTools](https://rapidlasso.com/).
+
+The script needs an elevated rights powershell (admin) and can be excuted with the following command:
+
+`powershell -executionpolicy bypass -File .\lastools_install_simple.ps1`
+
+The script downloads(to MyDocuments) the latest version from rapidlassos website, extracts it to C:\LASTools.
+Then it creates a link on the user Desktop to LASTools\bin.
+At last it checks the PATH var and if not presents adds "C:\LASTools\bin".
+
---
+++ lastools_install_simple.ps1
@@ -0,0 +1,81 @@
+# Lastools install script
+# Version 1.0
+# DF
+# 28.03.2018
+
+#################
+# var definiton
+#################
+
+# folders for batchscripts on odin
+$lastoolslicense = "\\path\2\lastoolslicense.txt"
+
+# LAStools download url and local install poth
+$lastoolsURL = "http://lastools.org/download/LAStools.zip"
+$lastoolsPathLocal = "c:\"
+
+# for PATH Variable change drive letter and path if needed
+$lastoolsfolder ="C:\LASTools"
+# reg key for getting acutal path vars
+$Reg = "Registry::HKLM\System\CurrentControlSet\Control\Session Manager\Environment"
+# lastools path as regex
+$regex = "[cC]\:\\LASTools"
+
+
+#######################################
+#
+# MAIN PROGRAMM DO NOT EDIT !
+#
+#######################################
+
+Write-host "`nLASTools installer & updater"
+Write-host "version 1.0"
+Write-host "28.03.2018"
+
+# Download latest Version
+# get MyDocuments path
+$downloads = [System.Environment]::GetFolderPath('MyDocuments')
+Write-host "`nDownloading LASTools to $downloads ..."
+wget $lastoolsURL -OutFile "$downloads\Lastools.zip"
+
+# Extract to c\:LASTools
+Write-host "`nExtracting LASTools to C:\LASTools\"
+Expand-Archive "$downloads\Lastools.zip" -DestinationPath $lastoolsPathLocal -Force
+
+# function for creating desktop link
+function createLinkonDesktop
+{       
+        param([string]$lastoolsbatchfolder, [string]$batchfilelinkname)
+        Write-host "$lastoolsbatchfolder $batchfilelinkname"
+        $wshshell = New-Object -ComObject WScript.Shell
+        $desktop = [System.Environment]::GetFolderPath('Desktop')
+        Write-host "$desktop\$batchfilelinkname.lnk" 
+        $lnk = $wshshell.CreateShortcut($desktop+"\"+$batchfilelinkname+".lnk")
+        $lnk.TargetPath = "$lastoolsbatchfolder"
+        $lnk.Save() 
+}
+
+# creating desktop links
+Write-host "`nGenerating link to LASTools on Desktop"
+createLinkonDesktop $lastoolsfolder"\bin" "LAStools"
+
+# add path var for lastools
+# get existing path vars
+$OldPath = (Get-ItemProperty -Path "$Reg" -Name PATH).Path
+
+# checking for exisiting entry
+# if not write it
+if ( -Not ($OldPath -match $regex))
+    {
+    Write-host "`nSetting Path Variable to $AddedLocation"
+    $NewPath= $OldPath + ';' + $AddedLocation
+    Set-ItemProperty -Path "$Reg" -Name PATH -Value $NewPath
+    }
+
+# if already exist skip the step
+if ($OldPath -match $regex)
+    {
+        Write-host "`nPATH Variable already set!"
+    }
+
+Write-host "`nScript finished!`n"
