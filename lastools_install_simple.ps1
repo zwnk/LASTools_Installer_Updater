7de29b332e0fe63745a1c40419d40919fc29433e
David Friedrich <david.friedrich@digimapas.cl>
2018-04-27 10:08:34 -0300

    check for running LAStools instance added

--- lastools_install_simple.ps1
+++ lastools_install_simple.ps1
@@ -1,7 +1,7 @@
 # Lastools install script
-# Version 1.0
+# Version 1.01
 # DF
-# 28.03.2018
+# 27.04.2018
 
 #################
 # var definiton
@@ -21,6 +21,8 @@ $Reg = "Registry::HKLM\System\CurrentControlSet\Control\Session Manager\Environm
 # lastools path as regex
 $regex = "[cC]\:\\LASTools"
 
+# LAStools filename
+$activeLAStools = "*las*"
 
 #######################################
 #
@@ -29,56 +31,65 @@ $regex = "[cC]\:\\LASTools"
 #######################################
 
 Write-host "`nLASTools installer & updater"
-Write-host "version 1.0"
-Write-host "28.03.2018"
+Write-host "version 1.01"
+Write-host "27.04.2018"
 
-# Download latest Version
-# get MyDocuments path
-$downloads = [System.Environment]::GetFolderPath('MyDocuments')
-Write-host "`nDownloading LASTools to $downloads ..."
-wget $lastoolsURL -OutFile "$downloads\Lastools.zip"
+# checking for running LAStools instance
+if( ($p = (Get-Process -ErrorAction SilentlyContinue -Name $activeLAStools)) -eq $null)
+{
 
-# Extract to c\:LASTools
-Write-host "`nExtracting LASTools to C:\LASTools\"
-Expand-Archive "$downloads\Lastools.zip" -DestinationPath $lastoolsPathLocal -Force
+    # Download latest Version
+    # get MyDocuments path
+    $downloads = [System.Environment]::GetFolderPath('MyDocuments')
+    Write-host "`nDownloading LASTools to $downloads ..."
+    wget $lastoolsURL -OutFile "$downloads\Lastools.zip"
+    
+    # Extract to c\:LASTools
+    Write-host "`nExtracting LASTools to C:\LASTools\"
+    Expand-Archive "$downloads\Lastools.zip" -DestinationPath $lastoolsPathLocal -Force
+    
+    # copy license file to LAStools\bin
+    copy-item $lastoolslicense $lastoolsPathLocal"\LASTools\bin"
+    
+    # function for creating desktop link
+    function createLinkonDesktop
+    {       
+            param([string]$lastoolsbatchfolder, [string]$batchfilelinkname)
+            Write-host "$lastoolsbatchfolder $batchfilelinkname"
+            $wshshell = New-Object -ComObject WScript.Shell
+            $desktop = [System.Environment]::GetFolderPath('Desktop')
+            Write-host "$desktop\$batchfilelinkname.lnk" 
+            $lnk = $wshshell.CreateShortcut($desktop+"\"+$batchfilelinkname+".lnk")
+            $lnk.TargetPath = "$lastoolsbatchfolder"
+            $lnk.Save() 
+    }
+    
+    # creating desktop links
+    Write-host "`nGenerating link to LASTools on Desktop"
+    createLinkonDesktop $lastoolsfolder"\bin" "LAStools"
+    
+    # add path var for lastools
+    # get existing path vars
+    $OldPath = (Get-ItemProperty -Path "$Reg" -Name PATH).Path
+    
+    # checking for exisiting entry
+    # if not write it
+    if ( -Not ($OldPath -match $regex))
+        {
+        Write-host "`nSetting Path Variable to $AddedLocation"
+        $NewPath= $OldPath + ';' + $AddedLocation
+        Set-ItemProperty -Path "$Reg" -Name PATH -Value $NewPath
+        }
+    
+    # if already exist skip the step
+    if ($OldPath -match $regex)
+        {
+            Write-host "`nPATH Variable already set!"
+        }
+    
+    Write-host "`nScript finished!`n"
 
-# copy license file to LAStools\bin
-copy-item $lastoolslicense $lastoolsPathLocal"\LASTools\bin"
-
-# function for creating desktop link
-function createLinkonDesktop
-{       
-        param([string]$lastoolsbatchfolder, [string]$batchfilelinkname)
-        Write-host "$lastoolsbatchfolder $batchfilelinkname"
-        $wshshell = New-Object -ComObject WScript.Shell
-        $desktop = [System.Environment]::GetFolderPath('Desktop')
-        Write-host "$desktop\$batchfilelinkname.lnk" 
-        $lnk = $wshshell.CreateShortcut($desktop+"\"+$batchfilelinkname+".lnk")
-        $lnk.TargetPath = "$lastoolsbatchfolder"
-        $lnk.Save() 
+} else {
+    Write-host "`nAbort Mission!!!!1111"
+    Write-host "`n $p is running"
 }
-
-# creating desktop links
-Write-host "`nGenerating link to LASTools on Desktop"
-createLinkonDesktop $lastoolsfolder"\bin" "LAStools"
-
-# add path var for lastools
-# get existing path vars
-$OldPath = (Get-ItemProperty -Path "$Reg" -Name PATH).Path
-
-# checking for exisiting entry
-# if not write it
-if ( -Not ($OldPath -match $regex))
-    {
-    Write-host "`nSetting Path Variable to $AddedLocation"
-    $NewPath= $OldPath + ';' + $AddedLocation
-    Set-ItemProperty -Path "$Reg" -Name PATH -Value $NewPath
-    }
-
-# if already exist skip the step
-if ($OldPath -match $regex)
-    {
-        Write-host "`nPATH Variable already set!"
-    }
-
-Write-host "`nScript finished!`n"
